b3e7e4db7c31a7895269ed8de5674d8a5f301e49
nieves13 <nievesaracil@gmail.com>
2019-10-16 17:43:00 +0200

    Numero aleatorio entre 10 y 50

--- src/Aciertos.java
+++ src/Aciertos.java
@@ -4,7 +4,7 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random()*(50-10+1)+10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
